4f97e2fc446d6fddd1a7094b21c49056b6005541
DatoXx8 <datoxx8@gmail.com>
2024-10-30 13:01:16 +0100

    FIX: Include pcg files for compile step

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-zig cc ./tensor.c ./tests/simulate-ops-cpu.c -o simulate-ops-cpu -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
+zig cc ./tensor.c ./tests/simulate-ops-cpu.c ./prng/pcg.c -o simulate-ops-cpu -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
 ./simulate-ops-cpu
-zig cc ./tensor.c ./tests/simulate-linearized.c -o simulate-linearized -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
+zig cc ./tensor.c ./tests/simulate-linearized.c ./prng/pcg.c -o simulate-linearized -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
 ./simulate-linearized 1000 10
 zig cc ./tensor.c ./compiler/codegen.c ./compiler/compile.c ./runtimes/cl.c ./tests/simulate-compiler.c ./utils.c ./prng/pcg.c -o simulate-compiler -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
 ./simulate-compiler
